578565c1fb4157cec801ef21643272388ce74cb9
patrikcsak <95058980+patrikcsak@users.noreply.github.com>
2023-04-24 20:33:54 +0200

    RegisterController - Create

---
+++ Project/Nyilvantarto_alkalmazas/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('register');
+    }
+
+    public function store(Request $request)
+    {
+        
+        $request->validate([
+                'nickname'=> 'required|min:3|max:10',
+                'firstname'=> 'required|min:3|max:10',
+                'lastname'=> 'required|min:3|max:10',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:8|max:15' ,
+            ]);
+
+        $user = new User();
+        $user->nickname = $request->nickname;
+        $user->firstname = $request->firstname;
+        $user->lastname = $request->lastname;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $res = $user->save();
+
+        if($res)
+        {
+            return back()->with('success','Sikeresen regisztrált!');
+        }
+        else
+        {
+            return back()->with('fail', 'Valami nincs rendben!');
+        }
+    }
+}
