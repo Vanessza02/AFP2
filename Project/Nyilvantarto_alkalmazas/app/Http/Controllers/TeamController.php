c9ac3c9d70fd2d89cc15f5f321e04914da758f2a
patrikcsak <95058980+patrikcsak@users.noreply.github.com>
2023-05-09 17:06:25 +0200

    TeamController - Csapat létrehozása
    Itt a team és a teammember adatbázisokba hozzáadja az adatokat.

---
+++ Project/Nyilvantarto_alkalmazas/app/Http/Controllers/TeamController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Team;
+use App\Models\TeamMember;
+
+class TeamController extends Controller
+{
+    public function store(Request $request){
+        
+        $user = Auth::user();
+        $user_id = $user->id;
+
+        // Az űrlap adatainak ellenőrzése
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+    
+        $team = new Team;
+        $team->user_id = $user_id;
+        $team->name = $request->name;
+        $team->description = $request->description;
+        $res = $team->save();
+
+        if($res)
+        {
+            $teamMember = new TeamMember;
+            $teamMember->team_id = $team->id;
+            $teamMember->user_id = $user_id;
+            $teamMember->role = "Csapat vezető";
+            $teamMember->save();
+
+            return back()->with('success','Sikeresen létrehoztál egy új csapatot!');
+        }
+        else
+        {
+            return back()->with('fail', 'Valami nincs rendben!');
+        }
+    }
+
+    public function index(){
+        return view('Dashboard.Team.team');
+    }
+
+}
