7546e95498b6f748b1b71d8a4738b5e0e6389c35
Vanessza02 <114110363+Vanessza02@users.noreply.github.com>
2023-05-09 10:44:45 +0200

    megjegyzés adatbázis modellje

---
+++ Project/Nyilvantarto_alkalmazas/app/Models/megjegyzes.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class megjegyzes extends Model
+{
+    use HasFactory;
+
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function commentable() {
+        return $this->morphTo();
+    }
+    
+    public function replies() {
+        return $this->morphMany(Comment::class, 'commentable')
+            ->orderBy('created_at', 'desc');
+    }
+    
+    public function getIsReplyAttribute() {
+        return $this->commentable_type == 'comment';
+    }
+
+
+}
+
+
+
+
